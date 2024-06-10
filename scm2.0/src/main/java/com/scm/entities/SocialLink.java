b25b4610974e1b502a83cb9ad884e4642f46372c
Ansar Ahmad <ansar.ahmad@dxc.com>
2024-06-10 17:25:58 +0530

    Designed Signup form

--- scm2.0/src/main/java/com/scm/entities/SocialLink.java
+++ scm2.0/src/main/java/com/scm/entities/SocialLink.java
@@ -1,9 +1,15 @@
 package com.scm.entities;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "tbl_social_link")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SocialLink {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
